b27bd84d70e50fbca1bf6e7805ed3c6f55bb89cd
Carlos Dev <carlosmpr.ing.sistemas@gmail.com>
2026-05-18 15:55:13 -0400

    Adicion del modelo para pacientes donde tendremos los campos que se requiren ademas de metodos para guardar actulizar y eliminar pacientes, tambien para obtener todos los pacientes y un solo paciente por id

---
+++ models/paciente_model.py
@@ -0,0 +1,45 @@
+from database import db
+
+class Paciente(db.Model):
+    __tablename__ = "pacientes"
+    
+    id_medico = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(80), nullable=False)
+    edad = db.Column(db.Integer, nullable=False)
+    direccion = db.Column(db.String(200))
+    telefono = db.Column(db.Integer, nullable=False)
+    
+    
+    def __init__(self, nombre, edad, direccion, telefono):
+        self.nombre = nombre
+        self.edad = edad
+        self.direccion = direccion
+        self.telefono = telefono
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    @staticmethod
+    def get_all():
+        return Paciente.query.all()
+    
+    @staticmethod
+    def get_by_id(id):
+        return Paciente.query.get(id)
+    
+    def update(self, nombre=None, edad=None, direccion=None, telefono=None):
+        if nombre:
+            self.nombre = nombre
+        if edad:
+            self.edad = edad
+        if direccion:
+            self.direccion = direccion
+        if telefono:
+            self.telefono = telefono
+            
+        db.session.commit()
+            
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
