3f0f1a6c87b24bce13c89ed1fbd89af3a797db59
Carlos Dev <carlosmpr.ing.sistemas@gmail.com>
2026-05-18 15:37:23 -0400

    Creamos la base de datos y las tablas con app.app.context() ademas de registrar los bluprints en este caso el que definimos para medicos es medico_bp

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from database import db
+from controllers import medico_conroller
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqllite:///clinica.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///clinica.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+app.register_blueprint(medico_conroller.medico_bp)
 
 @app.route("/")
 def home():
     return "<h1>Aplicacion Medic</h1>"
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
