7e9af9af5dd198dcb5f31318b27e707fe13a5aa0
Carlos Dev <carlosmpr.ing.sistemas@gmail.com>
2026-05-18 19:03:58 -0400

    Creacion del controlador para paciente, donde estara nuestro blueprint llamado paciente con el prefijo pacientes, metodos para listar pacientes, crear un paciente, actualizar un paciente y finalmente la eliminacion de un paciente. Todo esto haciendo de los metodos creaados en el modelo, y redirecciones cargando las vistas en este caso paciente_views

---
+++ controllers/paciente_controller.py
@@ -0,0 +1,43 @@
+from flask import request, url_for, redirect, Blueprint
+from models.paciente_model import Paciente 
+from views import paciente_views
+
+paciente_bp = Blueprint("paciente", __name__, url_prefix="/pacientes")
+
+@paciente_bp.route("/")
+def index():
+    pacientes = Paciente.get_all()
+    return paciente_views.list_patients(pacientes)
+
+@paciente_bp.route("/create" , methods=["GET", "POST"])
+def create_patient():
+    if request.method == "POST":
+        nombre = request.form['nombre']
+        edad = request.form['edad']
+        direccion = request.form['direccion']
+        telefono = request.form['telefono']
+        
+        paciente = Paciente(nombre,edad,direccion,telefono)
+        paciente.save()
+        return redirect(url_for("paciente.index"))
+    return paciente_views.create_patient()
+
+@paciente_bp.route("/edit/<int:id_patient>", methods=["GET", "POST"])
+def edit_patient(id_patient):
+    paciente = Paciente.get_by_id(id_patient)
+    if request.method == "POST":
+        nombre = request.form['nombre']
+        edad = request.form['edad']
+        direccion = request.form['direccion']
+        telefono = request.form['telefono']
+        
+        paciente.update(nombre=nombre, edad=edad, direccion=direccion, telefono=telefono)
+        return redirect(url_for("paciente.index"))
+    
+    return paciente_views.edit_patient(paciente)     
+
+@paciente_bp.route("/delete/<int:id_patient>")
+def delete_patient(id_patient):
+    paciente = Paciente.get_by_id(id_patient)
+    paciente.delete()
+    return redirect(url_for("paciente.index"))  
