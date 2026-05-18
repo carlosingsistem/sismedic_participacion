0098124ca99c340d38edc79970f1f7ef4686b386
Carlos Dev <carlosmpr.ing.sistemas@gmail.com>
2026-05-18 19:09:51 -0400

    Modificacion de la plantilla base base.html adicion de las rutas. Adicion de la base de datos

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from database import db
-from controllers import medico_conroller
+from controllers import medico_conroller, paciente_controller
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///clinica.db"
@@ -8,6 +8,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 app.register_blueprint(medico_conroller.medico_bp)
+app.register_blueprint(paciente_controller.paciente_bp)
 
 @app.route("/")
 def home():
