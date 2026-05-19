25ccc07be7ab782151d1e16b1b574862e2c92b36
Carlos Dev <carlosmpr.ing.sistemas@gmail.com>
2026-05-19 16:20:53 -0400

    Modificacion del metodo list_consults pasandole tambien la fehca de inicio y final para la renderizacion

--- views/consulta_views.py
+++ views/consulta_views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 
-def list_consults(consultas):
-    return render_template("consultas/index.html" , consultas=consultas)
+def list_consults(consultas, fecha_inicio, fecha_fin):
+    return render_template("consultas/index.html" , consultas=consultas, fecha_inicio=fecha_inicio, fecha_fin=fecha_fin)
     
 def create_consult(medicos, pacientes):
     return render_template("consultas/create.html", medicos=medicos, pacientes=pacientes)
