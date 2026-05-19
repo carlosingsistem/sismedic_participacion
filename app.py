9aee2abd5a12c5c58b0af480e46cf5720e5e63cc
Carlos Dev <carlosmpr.ing.sistemas@gmail.com>
2026-05-19 02:07:10 -0400

    Adicion de consultas en templates que tendra los archivos que s renderizaron en las vistas, seran para listar(index.html), crear(create.html) y editar(edit.html) consultas ademas de la eliminaion de estas

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from database import db
-from controllers import medico_conroller, paciente_controller
+from controllers import medico_conroller, paciente_controller, consulta_controller
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///clinica.db"
@@ -9,10 +9,17 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 app.register_blueprint(medico_conroller.medico_bp)
 app.register_blueprint(paciente_controller.paciente_bp)
+app.register_blueprint(consulta_controller.consulta_bp)
 
 @app.route("/")
 def home():
     return "<h1>Aplicacion Medic</h1>"
+
+@app.context_processor
+def inject_active_route():
+    def is_active(path):
+        return "active" if request.path == path else ''
+    return dict(is_active = is_active)
 
 if __name__ == "__main__":
     with app.app_context():
